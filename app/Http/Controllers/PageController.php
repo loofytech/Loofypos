54091ae4dd3a14848fec7fbba28fb17efcc76d27
agungd3v <agungd3v@gmail.com>
2023-09-20 18:45:28 +0700

    update

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\MyHelper;
+use App\Models\Selling;
+use App\Models\Store;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie;
 
 class PageController extends Controller
 {
     public function dashboard() {
-        return view('dashboard.index');
+        $store = Store::where('store_slug', MyHelper::getActiveStore())->first();
+        $selling = Selling::where('store_id', $store->id)->whereDate('created_at', Carbon::today())->sum('total');
+        return view('dashboard.index', compact('selling'));
     }
 
     public function product() {
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,33 +3,35 @@
 
 @section('content')
 <div class="row">
-  <div class="col-lg-8 mb-4 order-0">
+  <div class="col-lg-3 col-md-12 col-6">
     <div class="card">
-      <div class="d-flex align-items-end row">
-        <div class="col-sm-7">
-          <div class="card-body">
-            <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
-            <p class="mb-4">
-              You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
-              your profile.
-            </p>
-
-            <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
+      <div class="card-body">
+        <div class="card-title d-flex align-items-start justify-content-between">
+          <div class="avatar flex-shrink-0">
+            <img src="{{ asset('assets/img/icons/unicons/cc-primary.png') }}" alt="Credit Card" class="rounded">
+          </div>
+          <div class="dropdown">
+            <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="bx bx-dots-vertical-rounded"></i>
+            </button>
+            <div class="dropdown-menu dropdown-menu-end" style="">
+              <a class="dropdown-item" href="{{ route('selling', LoofyHelper::getActiveStore()) }}">Selengkapnya</a>
+            </div>
           </div>
         </div>
-        <div class="col-sm-5 text-center text-sm-left">
-          <div class="card-body pb-0 px-0 px-md-4">
-            <img
-              src="{{ asset('assets/img/illustrations/man-with-laptop-light.png') }}"
-              height="140"
-              alt="View Badge User"
-              data-app-dark-img="{{ asset('illustrations/man-with-laptop-dark.png') }}"
-              data-app-light-img="{{ asset('illustrations/man-with-laptop-light.png') }}"
-            />
-          </div>
-        </div>
+        <span class="fw-semibold d-block mb-1">Penjualan hari ini</span>
+        <h3 class="card-title mb-0" id="selling" data-selling="{{ $selling }}">Rp 0</h3>
+        {{-- <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small> --}}
       </div>
     </div>
   </div>
 </div>
 @endsection
+
+@push('js')
+<script>
+  $(document).ready(function() {
+    $('#selling').text('Rp ' + $('#selling').data('selling').toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1."));
+  });
+</script>
+@endpush
